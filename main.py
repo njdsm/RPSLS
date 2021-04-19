1ee7b9ea79b9cebb62f245dd87a483ceaf185a1b
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-04-19 16:33:37 -0500

    added replay functionality

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from run import Run
 
 if __name__ == '__main__':
-    game = Run()
-    game.run_game()
+    again = True
+    while again:
+        game = Run()
+        game.run_game()
+        user_input = input("\n\n\nWould you like to play again? y/n\n:")
+        if user_input != "y":
+            again = False
